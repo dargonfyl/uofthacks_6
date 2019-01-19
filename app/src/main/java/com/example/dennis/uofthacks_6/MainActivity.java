a8aa29cc9713c2c2a7b7ee61ea834fbc0e693820
Mian Wang <mian.wang@mail.utoronto.ca>
2019-01-19 11:28:57 -0500

    Created project

---
+++ app/src/main/java/com/example/dennis/uofthacks_6/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dennis.uofthacks_6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivi ty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
