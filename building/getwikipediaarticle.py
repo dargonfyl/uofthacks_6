38a22821dc690b8f786cc01c8165e90ec5d58cfb
Victor <victor.lu@mail.utoronto.ca>
2019-01-19 18:23:54 -0500

    cleaned up wikpedia article

--- building/getwikipediaarticle.py
+++ building/getwikipediaarticle.py
@@ -12,8 +12,11 @@ class WikiPage:
 
     def __init__(self, entry_name: str):
         self.entry_name = entry_name
-        self.success = 0
-        self.entry = self.parse_page(entry_name)
+        try:
+            self.entry = self.parse_page(entry_name)
+        except Exception:
+            raise WikiPageException("Wikipedia Article Error!")
+        self.summary_paragraph_counter = 0
 
     def parse_page(self, entry_name):
         """
@@ -21,61 +24,88 @@ class WikiPage:
         """
         try:
             entry = wikipedia.page(entry_name)
-            self.success = 1
         except wikipedia.exceptions.DisambiguationError as e:
             entry = wikipedia.page(e.options[0])
-            self.success = 1
         except wikipedia.exceptions.PageError as e:
             print(e)
-            return None
+            raise e
         except wikipedia.exceptions.RedirectError as e:
             print(e)
-            return None
+            raise e
         except wikipedia.exceptions.HTTPTimeoutError as e:
             print(e)
-            return None
+            raise e
         except Exception as e:
             print(e)
-            return None
+            raise e
         return entry
 
     def get_summary_full(self):
         """
         Return summary of wiki article
         """
-        if self.success == 1:
-            return self.entry.summary.strip()
+        return self.entry.summary.strip()
+
+    def get_summary_first_paragraph(self):
+        """
+        Return first paragraph of wiki article
+        """
+        self.summary_paragraph_counter += 1
+        return self.entry.summary.split("\n")[0]
+
+    def get_summary_next_paragraph(self):
+        """
+        Returns paragraph number self.summary_paragraph_counter
+        """
+        max = len(self.entry.summary.split("\n"))
+        if self.summary_paragraph_counter < max:
+            ret = self.entry.summary.split("\n")[self.summary_paragraph_counter]
+            while len(ret) == 0:
+                self.summary_paragraph_counter += 1
+                ret = self.entry.summary.split("\n")[self.summary_paragraph_counter]
+            self.summary_paragraph_counter += 1
+            return ret
         else:
-            raise WikiPageException("Wikipedia Article Error!")
+            return "****     END OF SUMMARY -------- *********************"
 
     def get_url(self):
         """
         Returns url of wiki article
         """
-        if self.success == 1:
-            return self.entry.url
-        else:
-            raise WikiPageException("Wikipedia Article Error!")
+        return self.entry.url
 
     def get_img_url(self):
         """
         Gets image url. Empty string if no images
         """
-        if self.success == 1:
-            return self.entry.images[0] if len(self.entry.images) > 0 else ""
-        else:
-            raise WikiPageException("Wikipedia Article Error!")
+        return self.entry.images[0] if len(self.entry.images) > 0 else ""
+
+    def get_img_list(self):
+        """
+        Return a list of images urls found on the page.
+        """
+        return self.entry.images if len(self.entry.images) > 0 else ""
 
 
 class WikiPageException(Exception):
     """"uguu we made a fucky wucky"""
     pass
 
-#
-# if __name__ == "__main__":
-#     w = WikiPage("Fungus")
-#     print(w.get_img_url())
-#     # print(w.get_summary_full())
-#     # print(w.entry.content)
-#     # print(w.entry.section())
-#     print(w.entry.sections[0])
+
+if __name__ == "__main__":
+    w = WikiPage("Table")
+    print(w.get_img_url())
+    print(w.get_summary_next_paragraph())
+    print(w.get_summary_next_paragraph())
+    print(w.get_summary_next_paragraph())
+    print(w.get_summary_next_paragraph())
+    print(w.get_summary_next_paragraph())
+    print(w.get_summary_next_paragraph())
+    print(w.get_summary_next_paragraph())
+    print(w.get_summary_next_paragraph())
+    print(w.get_summary_next_paragraph())
+    print(w.get_summary_next_paragraph())
+    print(w.get_summary_next_paragraph())
+    # print(w.entry.content)
+    # print(w.entry.section())
+    # print(w.entry.sections)
