13e8a2815d3d152349dc5a0ec388ca8ed19b7df0
5DsDLTomorrow <shirleywang@hotmail.ca>
2019-01-19 16:44:16 -0500

    testing imagetoword

--- building-an-app/imagetoword.py
+++ building-an-app/imagetoword.py
@@ -1,2 +1,18 @@
 # Takes image url and gives the word for what it is
 
+import json
+from watson_developer_cloud import VisualRecognitionV3
+
+"""
+Input the url link of a picture and getWord will return the best classifier word
+for what the picture shows.
+"""
+def getWord(url):
+    visual_recognition = VisualRecognitionV3(
+        '2018-03-19',
+        iam_apikey='G_vhnFiNCnhp97Uew544e5aF0NfcNzkL-FRWcZ-L_uAq')
+
+    classes_result = visual_recognition.classify(url=url).get_result()
+    image = json.dumps(classes_result, indent=2)
+    return image['images'][0]['classifiers'][0]['classes'][0]['class']
+
